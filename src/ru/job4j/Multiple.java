87434d667079ad0eabaf71ec9fb6656e9986630a
AleksandrMo <sushanta@mail.ru>
2021-07-19 17:20:12 +0700

    3.3. Разбор первой программы. [#159033 #179226]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -4,6 +4,12 @@ public class Multiple {
     public static void main(String[] args) {
         System.out.println("1*2=2");
         System.out.println("1*3=3");
-        System.out.println("1*9=9");
+        System.out.println("1*4=9");
+        System.out.println("1*5=2");
+        System.out.println("1*6=3");
+        System.out.println("1*7=9");
+        System.out.println("1*8=2");
+        System.out.println("1*9=3");
+
     }
 }
