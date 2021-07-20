8bc95c0624380955aea67de689bf9a32a9213d90
AleksandrMo <sushanta@mail.ru>
2021-07-21 06:53:48 +0700

    Добавил тесты

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,9 +13,20 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        double dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " euros");
+        float dollar = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + euro + " euros");
         System.out.println("140 rubles are " + dollar + " dollars");
+
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        expected = 2.33333333f;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("140 dollars are 2,33333333. Test result : " + passed);
 
     }
 
