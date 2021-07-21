8000223d75f764359937618f4bb156048adcd7a5
AleksandrMo <sushanta@mail.ru>
2021-07-21 22:29:21 +0700

    1.2. Входные данные в тесте [#297750]

--- test/ru/job4j/condition/X2Test.java
+++ test/ru/job4j/condition/X2Test.java
@@ -22,7 +22,7 @@ public class X2Test {
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected = 40;
+        int expected = 3;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
@@ -33,7 +33,7 @@ public class X2Test {
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected = 40;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
@@ -44,7 +44,7 @@ public class X2Test {
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = 40;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
@@ -55,7 +55,7 @@ public class X2Test {
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = 40;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
