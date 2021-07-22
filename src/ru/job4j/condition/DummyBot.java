3185f78bc8016d896a58ca854bfe2134d9af1689
AleksandrMo <sushanta@mail.ru>
2021-07-22 12:25:19 +0700

    4.1.1. Операторы сравнения в String. [#237406]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 public class DummyBot {
 
     public static String answer(String question) {
+
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl =  "Привет, умник.";
--- test/ru/job4j/condition/DummyBotTest.java
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import org.junit.Assert;
 
 public class DummyBotTest {
