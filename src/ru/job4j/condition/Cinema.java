da90f91407e2d5a65e0f943ab2f5d5ed02adfb3d
AleksandrMo <sushanta@mail.ru>
2021-07-22 09:16:03 +0700

    4.1.0. Оператор if с блоком else [#227534]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Cinema {
 
-    public static void acces(int age) {
+    public static void access(int age) {
         System.out.println("The age of the customer is " + age);
         if (age >= 18) {
             System.out.println("Welcome to the cinema!");
@@ -12,7 +12,7 @@ public class Cinema {
     }
 
     public static void main(String[] args) {
-        Cinema.acces(13);
-        Cinema.acces(32);
+        Cinema.access(13);
+        Cinema.access(32);
     }
 }
