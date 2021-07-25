44c4fd6147078a5749a916e380cea24433b17ba6
AleksandrMo <sushanta@mail.ru>
2021-07-25 13:29:26 +0700

    Ипотека - сокротил код

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            double yearPlusPercent = amount + (amount * (percent / 100));
-            amount = yearPlusPercent - salary;
+            amount = amount + (amount * (percent / 100)) - salary;
             year += 1;
         }
         return year;
