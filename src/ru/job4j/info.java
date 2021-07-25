b7978607ee34ba60986937d31b870b7165f1aa9a
AleksandrMo <sushanta@mail.ru>
2021-07-25 11:12:11 +0700

    Класс с большой буквы

--- src/ru/job4j/info.java
+++ src/ru/job4j/info.java
@@ -1,10 +1,9 @@
 package ru.job4j;
 
-public class info {
+public class info { // класс написан с маленькой буквы
 
     public static void main(String[] args) {
 
         System.out.println("19.07.2021");
-
     }
 }
