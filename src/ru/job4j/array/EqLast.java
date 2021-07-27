8f1ba1de31ad2c806afb7d65d6e4a6fe833944a7
AleksandrMo <sushanta@mail.ru>
2021-07-27 12:43:25 +0700

    Сократил код

--- src/ru/job4j/array/EqLast.java
+++ src/ru/job4j/array/EqLast.java
@@ -3,11 +3,8 @@ package ru.job4j.array;
 public class EqLast {
 
     public static boolean check(int[] left, int[] right) {
-        boolean check = false;
-        if (left[left.length - 1] == right[right.length - 1]) {
-            check = true;
-        }
-        return check;
+
+        return left[left.length - 1] == right[right.length - 1];
     }
 
 }
