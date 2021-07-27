ca869259f4630c87bdf468052e102ab719478ef5
AleksandrMo <sushanta@mail.ru>
2021-07-27 12:31:46 +0700

    не получается перебрать

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data[data.length - 1]);
+            int index = FindLoop.indexOf1(data, min, i, data[data.length - 1]);
+            SwitchArray.swap(data, index, data[data.length - 1]);
+        }
+        return data;
+
+    }
+}
