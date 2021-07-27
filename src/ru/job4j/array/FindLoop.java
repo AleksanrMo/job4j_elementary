6c5f7fddb204aaf6c7df6a390e1a4421524bcb05
AleksandrMo <sushanta@mail.ru>
2021-07-27 13:26:43 +0700

    Исправил код

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -4,7 +4,7 @@ public class FindLoop {
 
     public static int indexOf(int[] data, int el) {
         int rsl = -1;
-       for (int i = 0; i <= data.length; i++) {
+       for (int i = 0; i < data.length; i++) {
            if (data[i] == el) {
                rsl = i;
                break;
