c3b3e437d66116b71d6cc1ebff0e3083d89d3c0f
AleksandrMo <sushanta@mail.ru>
2021-07-27 15:20:39 +0700

    Исправил код

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -4,7 +4,7 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, data.length - 1); // нашли мин число
+            int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexOf1(data, min, i, data.length - 1); //
             SwitchArray.swap(data, index, i);
 
