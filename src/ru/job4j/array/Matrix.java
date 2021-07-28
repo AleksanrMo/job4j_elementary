237442643d70e6d6b117f1deebdae66aa943c412
AleksandrMo <sushanta@mail.ru>
2021-07-28 10:54:03 +0700

    поправил код

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class Matrix {
 
     public static int[][] multiple(int size) {
-       int[][] multipleTable = new int[size + 1][size + 1];
-        for (int row = 1; row < multipleTable.length; row++) {
-            for (int cell = 1; cell < multipleTable[row].length; cell++) {
-                multipleTable[row][cell] = row * cell;
+       int[][] multipleTable = new int[size][size];
+        for (int row = 0; row < multipleTable.length; row++) {
+            for (int cell = 0; cell < multipleTable[row].length; cell++) {
+                multipleTable[row][cell] = (row + 1) * (cell + 1);
 
             }
         }
