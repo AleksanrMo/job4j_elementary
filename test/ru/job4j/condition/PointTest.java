62a6af119afc1a74ccc379dddfeb7cafa33177dc
AleksandrMo <sushanta@mail.ru>
2021-08-02 12:45:47 +0700

    не работает

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -3,55 +3,43 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class PointTest {
 
-  /*  @Test
-    public void when00to20then2() {
-        double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-
+    @Test
+    public void  when10to21then2() {
+        Point a = new Point(2, 1);
+        Point b = new Point(1, 0);
+        double out = a.distance(b);
+        double expected =  1.41;
+       Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when01to20then2() {
-        double expected = 2.23;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+    public void   when00to21then2() {
+        Point a = new Point(2, 1);
+        Point b = new Point(0, 0);
+        double out = a.distance(b);
+        double expected =  2.23;
         Assert.assertEquals(expected, out, 0.01);
-
     }
 
     @Test
-    public void when00to21then2() {
-        double expected = 2.23;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
+    public void   when00to20then2() {
+        Point a = new Point(2, 0);
+        Point b = new Point(0, 0);
+        double out = a.distance(b);
+        double expected =  2;
         Assert.assertEquals(expected, out, 0.01);
-
     }
 
     @Test
-    public void when10to21then2() {
-        double expected = 1.41;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
+    public void   when00to202then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point z = new Point(3, 0);
+        double out = a.distance3d(z);
+        double expected =  3.6;
         Assert.assertEquals(expected, out, 0.01);
+    }
 
-    }*/
 }
