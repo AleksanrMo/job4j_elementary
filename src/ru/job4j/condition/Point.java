27d65a4d5707e49d56aa962debff73e827540f8c
AleksandrMo <sushanta@mail.ru>
2021-08-03 17:22:45 +0700

    сократил код

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -25,7 +25,6 @@ public class Point {
      }
 
     public double distance3d(Point that) {
-        distance(that);
         return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2) +  Math.pow((this.z - that.z), 2));
     }
 
